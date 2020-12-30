f957658da7a659cb41ea8503299503b3cac46260
Anders Nygren <anders.nygren@jayway.com>
2020-12-30 10:37:37 +0100

    Add more hungry players

---
+++ scripts/hungry-player4.js
@@ -0,0 +1,48 @@
+const http = require("http");
+
+async function getUrl(url) {
+  return new Promise((resolve, reject) => {
+    http.get(url, res => {
+      let content = "";
+      res.setEncoding("utf-8");
+      res.on("data", chunk => (content += chunk));
+      res.on("end", () => resolve(JSON.parse(content)));
+    });
+  });
+}
+
+async function wait(time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, time);
+  });
+}
+
+function makeMove({ width, height, snake, fruit }) {
+  const [x, y] = snake[0];
+  const [fx, fy] = fruit;
+
+  if (fx < x) return "WEST";
+  if (fx > x) return "EAST";
+  if (fy < y) return "SOUTH";
+  if (fy > y) return "NORTH";
+  return "I DONT KNOW";
+}
+
+async function main() {
+  let data = await getUrl("http://localhost:3000/start/oskar");
+  while (true) {
+    await wait(100);
+    if (data.alive) {
+      const direction = makeMove(data);
+      console.log("MOVING", direction);
+      data = await getUrl(
+        `http://localhost:3000/move/oskar?direction=${direction}`
+      );
+      if (data.alive === false) console.log("died");
+    } else {
+      data = await getUrl("http://localhost:3000/start/oskar");
+    }
+  }
+}
+
+main();
